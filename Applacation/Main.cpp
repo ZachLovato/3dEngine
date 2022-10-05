872acd5c30f60eb3eedc058f540e420f4261c938
ZachLovato <zlovato@student.neumont.edu>
2022-10-05 15:34:53 -0600

    Got OpenGL into project

--- Applacation/Main.cpp
+++ Applacation/Main.cpp
@@ -1,7 +1,7 @@
 #include "Engine.h"
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
 	std::cout << "bork\n";
 
@@ -10,7 +10,7 @@ int main()
 	neu::Engine::Instance().Initialize();
 	neu::Engine::Instance().Register();
 
-	neu::g_renderer.CreateWindow("Neumont", 800, 600);
+	neu::g_renderer.CreateWindow("Bork", 800, 600);
 
 	bool quit = false;
 	while (!quit)
@@ -20,9 +20,22 @@ int main()
 		if (neu::g_inputSystem.GetKeyState(neu::key_escape) == neu::InputSystem::KeyState::Pressed) quit = true;
 
 		neu::g_renderer.BeginFrame();
+
+		glBegin(GL_TRIANGLES);
+
+		glColor3f(1.0f, 0.0f, 0.3f);
+		glVertex2f(-0.5f, -0.5f);
+		glColor3f(3.0f, 1.0f, 0.0f);
+		glVertex2f(0.0f, 0.5f);
+		glColor3f(0.0f, 0.3f, 1.0f);
+		glVertex2f(0.5f, -0.1f);
+
+		glEnd();
+
 		neu::g_renderer.EndFrame();
 	}
 
 
 	neu::Engine::Instance().Shutdown();
+	return 0;
 }
