0332a5d67cbd3de4b2674bb5bc8be746df86882c
ZachLovato <zlovato@student.neumont.edu>
2022-10-11 15:30:38 -0600

    Square

--- Applacation/Main.cpp
+++ Applacation/Main.cpp
@@ -1,16 +1,70 @@
 #include "Engine.h"
 #include <iostream>
 
+float points[] = {
+   -0.5f, -0.5f,  0.0f,
+   0.5f, 0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+
+   -0.5f, -0.5f,  0.0f,
+   -0.5f, 0.5f,  0.0f,
+   0.5f, 0.5f,  0.0f
+};
+
+const char* vertex_shader =
+"#version 430 core\n"
+"in vec3 position;"
+"void main() {"
+"  gl_Position = vec4(position * 0.5, 0.5);"
+"}";
+
+
+
+const char* fragment_shader =
+"#version 430 core\n"
+"out vec4 color;"
+"void main() {"
+"  color = vec4(0.5, 0.34, 1.0, 1.0);"
+"}";
+
 int main(int argc, char** argv)
 {
-	std::cout << "bork\n";
-
 	neu::InitializeMemory();
 
 	neu::Engine::Instance().Initialize();
 	neu::Engine::Instance().Register();
 
 	neu::g_renderer.CreateWindow("Bork", 800, 600);
+	
+	// create vertex buffer
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(float), points, GL_STATIC_DRAW);
+
+	// create vertex array
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// create shader
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	// create program
+	GLuint program = glCreateProgram();
+	glAttachShader(program, fs);
+	glAttachShader(program, vs);
+	glLinkProgram(program);
+	glUseProgram(program);
+
 
 	bool quit = false;
 	while (!quit)
@@ -21,16 +75,9 @@ int main(int argc, char** argv)
 
 		neu::g_renderer.BeginFrame();
 
-		glBegin(GL_TRIANGLES);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-		glColor3f(1.0f, 0.0f, 0.3f);
-		glVertex2f(-0.5f, -0.5f);
-		glColor3f(3.0f, 1.0f, 0.0f);
-		glVertex2f(0.0f, 0.5f);
-		glColor3f(0.0f, 0.3f, 1.0f);
-		glVertex2f(0.5f, -0.1f);
-
-		glEnd();
+		
 
 		neu::g_renderer.EndFrame();
 	}
