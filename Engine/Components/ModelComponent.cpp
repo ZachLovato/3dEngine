46a3d658a033f182b2eeb40b3223d8a46e79bbab
ZachLovato <zlovato@student.neumont.edu>
2022-11-15 16:42:56 -0700

    in class extra stuff

--- Engine/Components/ModelComponent.cpp
+++ Engine/Components/ModelComponent.cpp
@@ -12,9 +12,8 @@ namespace wrap
 		material->Bind();
 		// set model view projection matrix for model 
 		material->GetProgram()->SetUniform("model", (glm::mat4)m_owner->m_transform);
-		//material->GetProgram()->SetUniform("view", renderer.GetView());
-		//material->GetProgram()->SetUniform("projection", renderer.GetProjection());
 
+		glDepthMask(depth_test);
 		model->m_vertexBuffer.Draw();
 	}
 
@@ -36,6 +35,8 @@ namespace wrap
 		READ_DATA(value, material_name);
 		// get material from material name 
 		material = g_resources.Get<wrap::Material>(material_name);
+
+		READ_DATA(value, depth_test);
 
 		return true;
 	}
--- Engine/Components/ModelComponent.h
+++ Engine/Components/ModelComponent.h
@@ -22,5 +22,6 @@ namespace wrap
 	public:
 		std::shared_ptr<Model> model;
 		std::shared_ptr<Material> material;
+		bool depth_test = true;
 	};
 }
